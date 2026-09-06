6f4e2cc8b16bd81448851358a5ac50f8ce98da08
Yogesh Verma <avvermabgmi2021@gmail.com>
2026-09-06 19:43:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', // <-- GitHub Pages ke liye isko exact './' kar dijiye
-})
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />
+
+    <meta
+      name="description"
+      content="Yogesh Verma - Frontend Developer Portfolio"
+    />
+
+    <title>Yogesh Verma | Portfolio</title>
+  </head>
+
+  <body>
+    <div id="root"></div>
+
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
